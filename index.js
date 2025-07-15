318c508f40f2a3d1d5a188eacae8877fec75534b
Ankitherma <hermaankit@gmail.com>
2025-07-15 13:10:45 +0530

    donee done

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("helllloooo woooord");
+
+
+// let number = 7;
+
+// if (number % 2 === 0) {
+//   console.log("Evennnnnnnnn");
+// } else {
+//   console.log("Odddddddddd");
+// }
+
+
+
+
